7c4e7c5930325c24cab3fa7a23442d8a9b78ce72
przemyslawmarkiewicz <przemyslaemarkiewicz97@gmail.com>
2020-05-08 12:20:13 +0200

    Proj 4/ DiGraph

---
+++ Project4/plot.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+import networkx as nx
+import numpy as np
+
+
+def digraph_plot(adj_matrix):
+    graph = nx.from_numpy_matrix(np.matrix(adj_matrix), create_using=nx.DiGraph)
+    layout = nx.circular_layout(graph)
+    options = {
+        'node_color': '#570000',
+        'node_size': 300,
+        'line_color': '#878787',
+        'font_color': '#f2f2f2',
+        'width': 0.25,
+        'with_labels': 1,
+    }
+    nx.draw(graph, layout, **options)
+    plt.show()
---
+++ Project4/random_digraph.py
@@ -0,0 +1,27 @@
+from numpy.random import choice
+from plot import digraph_plot
+
+
+class RandomDiGraph:
+    def __init__(self, n, p):
+        self.adj_matrix = [[0 for el in range(n)] for el in range(n)]
+        for idx_row, row in enumerate(self.adj_matrix):
+            for idx_col, col in enumerate(row):
+                if idx_row != idx_col:
+                    self.adj_matrix[idx_row][idx_col] = int(choice([0, 1], 1, p=(1 - p, p)))
+        self.adj_list = [[idx for idx, val in enumerate(el) if val] for el in self.adj_matrix]
+
+    def print(self):
+        print('Adjacency Matrix:')
+        for row in self.adj_matrix:
+            print(row)
+        print('\nAdjacency list:')
+        for idx, li in enumerate(self.adj_list):
+            print(f"{idx}: {li}")
+        print('\nIncidence Matrix:')
+
+
+if __name__ == "__main__":
+    graph = RandomDiGraph(12, 0.4)
+    graph.print()
+    digraph_plot(graph.adj_matrix)
