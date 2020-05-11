f38aaa85abfd0fd577b5f36691d9bcf7f5fb9e02
przemyslawmarkiewicz <przemyslaemarkiewicz97@gmail.com>
2020-05-11 14:09:49 +0200

    Finished zad1/Proj4

--- Project4/digraph.py
+++ Project4/digraph.py
@@ -6,26 +6,34 @@ class DiGraph:
     def __init__(self, size):
         self.adj_matrix = [[0 for el in range(size)] for el in range(size)]
         self.adj_list = [[] for el in range(size)]
-        # self.inc_matrix = inc_martix
+        self.inc_matrix = [[] for el in range(size)]
 
     def add_edge(self, start_v, end_v):
         self.adj_list[start_v].append(end_v)
         self.adj_matrix[start_v][end_v] = 1
+        for idx, el in enumerate(self.inc_matrix):
+            if idx == start_v:
+                el.append(1)
+            elif idx == end_v:
+                el.append(-1)
+            else:
+                el.append(0)
 
     def create_with_adj_matrix(self, adj_matrix):
         self.adj_matrix = adj_matrix
         self.adj_list = [[idx for idx, val in enumerate(el) if val] for el in adj_matrix]
+        self.inc_matrix = adj_matrix_to_incidence_matrix(adj_matrix)
 
     def create_with_adj_list(self, adj_list):
         self.adj_list = adj_list
         size = range(len(adj_list))
         self.adj_matrix = [[1 if i in el else 0 for i in size] for el in adj_list]
+        self.inc_matrix = adj_matrix_to_incidence_matrix(self.adj_matrix)
 
     def create_from_file(self, filename):
         with open(filename) as f:
             adj_matrix = [[int(num) for num in line.split(' ')] for line in f]
         self.create_with_adj_matrix(adj_matrix)
-
 
     def print(self):
         print('Adjacency Matrix:')
@@ -35,19 +43,40 @@ class DiGraph:
         for idx, li in enumerate(self.adj_list):
             print(f"{idx}: {li}")
         print('\nIncidence Matrix:')
+        for row in self.inc_matrix:
+            print(row)
+
+
+def adj_matrix_to_incidence_matrix(adj_matrix):
+    inc_matrix = [[] for el in range(len(adj_matrix))]
+    for idx_row, row in enumerate(adj_matrix):
+        for idx_col, col in enumerate(row):
+            if adj_matrix[idx_row][idx_col]:
+                for idx, el in enumerate(inc_matrix):
+                    if idx == idx_row:
+                        el.append(1)
+                    elif idx == idx_col:
+                        el.append(-1)
+                    else:
+                        el.append(0)
+    return inc_matrix
 
 
 if __name__ == '__main__':
     graph = DiGraph(5)
-    # adj_l = [
-    #     [1, 4],
-    #     [2],
-    #     [3, 0],
-    #     [1, 0],
-    #     [2, 3]
-    # ]
-    # graph.create_with_adj_list(adj_l)
-    graph.create_from_file('test.in')
+    adj_l = [
+        [1, 4],
+        [2],
+        [3, 0],
+        [1, 0],
+        [2, 3]
+    ]
+    graph.create_with_adj_list(adj_l)
+    # graph.create_from_file('test.in')
     # graph.add_edge(0, 3)
+    # graph.add_edge(4, 1)
+    # graph.add_edge(4, 0)
+    # graph.add_edge(2, 1)
+
     graph.print()
     digraph_plot(graph.adj_matrix)
--- Project4/random_digraph.py
+++ Project4/random_digraph.py
@@ -5,13 +5,22 @@ import random
 class RandomDiGraph:
     def __init__(self, n, p, is_weighted=False):
         self.adj_matrix = [[0 for el in range(n)] for el in range(n)]
+        self.inc_matrix = [[] for i in range(n)]
         for idx_row, row in enumerate(self.adj_matrix):
             for idx_col, col in enumerate(row):
                 if idx_row != idx_col:
                     if is_weighted:
-                        self.adj_matrix[idx_row][idx_col] = int(choice([0, random.randint(1, 10)], 1, p=(1 - p, p)))
+                        self.adj_matrix[idx_row][idx_col] = int(choice([0, random.randint(1, 20)], 1, p=(1 - p, p)))
                     else:
                         self.adj_matrix[idx_row][idx_col] = int(choice([0, 1], 1, p=(1 - p, p)))
+                    if self.adj_matrix[idx_row][idx_col]:
+                        for idx, el in enumerate(self.inc_matrix):
+                            if idx == idx_row:
+                                el.append(1)
+                            elif idx == idx_col:
+                                el.append(-1)
+                            else:
+                                el.append(0)
         self.adj_list = [[idx for idx, val in enumerate(el) if val] for el in self.adj_matrix]
 
     def print(self):
@@ -22,9 +31,11 @@ class RandomDiGraph:
         for idx, li in enumerate(self.adj_list):
             print(f"{idx}: {li}")
         print('\nIncidence Matrix:')
+        for row in self.inc_matrix:
+            print(row)
 
 
 if __name__ == "__main__":
-    graph = RandomDiGraph(5, 1, is_weighted=True)
+    graph = RandomDiGraph(10, 0.6)
     graph.print()
-    weighted_digraph_plot(graph.adj_matrix)
+    digraph_plot(graph.adj_matrix)
