8de0689f574eecda18b51b146ba285456590499b
Marek <mark-now@wp.pl>
2020-05-14 15:21:36 +0200

    zad2 update

--- Project4/zad2/digraph.py
+++ Project4/zad2/digraph.py
@@ -7,26 +7,34 @@ class DiGraph:
     def __init__(self, size):
         self.adj_matrix = [[0 for el in range(size)] for el in range(size)]
         self.adj_list = [[] for el in range(size)]
-        # self.inc_matrix = inc_martix
+        self.inc_matrix = [[] for el in range(size)]
 
     def add_edge(self, start_v, end_v):
         self.adj_list[start_v].append(end_v)
         self.adj_matrix[start_v][end_v] = 1
+        for idx, el in enumerate(self.inc_matrix):
+            if idx == start_v:
+                el.append(1)
+            elif idx == end_v:
+                el.append(-1)
+            else:
+                el.append(0)
 
     def create_with_adj_matrix(self, adj_matrix):
         self.adj_matrix = adj_matrix
         self.adj_list = [[idx for idx, val in enumerate(el) if val] for el in adj_matrix]
+        self.inc_matrix = adj_matrix_to_incidence_matrix(adj_matrix)
 
     def create_with_adj_list(self, adj_list):
         self.adj_list = adj_list
         size = range(len(adj_list))
         self.adj_matrix = [[1 if i in el else 0 for i in size] for el in adj_list]
+        self.inc_matrix = adj_matrix_to_incidence_matrix(self.adj_matrix)
 
     def create_from_file(self, filename):
         with open(filename) as f:
             adj_matrix = [[int(num) for num in line.split(' ')] for line in f]
         self.create_with_adj_matrix(adj_matrix)
-
 
     def print(self):
         print('Adjacency Matrix:')
@@ -36,6 +44,11 @@ class DiGraph:
         for idx, li in enumerate(self.adj_list):
             print(f"{idx}: {li}")
         print('\nIncidence Matrix:')
+        for row in self.inc_matrix:
+            print(row)
+
+
+
 
     def DFS_visit(self, v, d, f, t):
         t[0]+=1
@@ -84,7 +97,7 @@ class DiGraph:
 
         nodes_comp_sorted = sorted(dict(zip(nodes, comp)).items(), key=lambda kv: (kv[1], kv[0]))
 
-        print("Skladowe grafu:")
+        print("Silnie spojne skladowe grafu:")
         tmp = nodes_comp_sorted[0][1]
         for i in nodes_comp_sorted:
             if tmp == i[1]:
@@ -95,8 +108,22 @@ class DiGraph:
             tmp = i[1]
 
         return comp
-        
-        
+
+
+def adj_matrix_to_incidence_matrix(adj_matrix):
+    inc_matrix = [[] for el in range(len(adj_matrix))]
+    for idx_row, row in enumerate(adj_matrix):
+        for idx_col, col in enumerate(row):
+            if adj_matrix[idx_row][idx_col]:
+                for idx, el in enumerate(inc_matrix):
+                    if idx == idx_row:
+                        el.append(1)
+                    elif idx == idx_col:
+                        el.append(-1)
+                    else:
+                        el.append(0)
+    return inc_matrix
+
 if __name__ == '__main__':
     graph = DiGraph(5)
     # adj_l = [
